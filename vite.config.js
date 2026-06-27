00d80db76c63eeb0ee20be5d2645bae086b253ff
nakovskabisera <nakovskabisera@gmail.com>
2026-06-28 00:53:45 +0200

    wowza

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: ".",
+  root: "src",
   build: {
     emptyOutDir: true,
     assetsInlineLimit: 0,
