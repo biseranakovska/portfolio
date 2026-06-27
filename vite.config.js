4cce6f488467e217ec2738edd700d5debe58ee98
nakovskabisera <nakovskabisera@gmail.com>
2026-06-28 00:42:27 +0200

    fixing the github pages error

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+  root: ".",
   build: {
     emptyOutDir: true,
     assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // projectSingle: resolve(__dirname, "src/pages/projectSingle.html"),
+        automotiveWebsite: resolve(
+          __dirname,
+          "src/pages/automotiveWebsite.html",
+        ),
+        dockAndBramble: resolve(__dirname, "src/pages/dockAndBramble.html"),
+        gamingDashboard: resolve(__dirname, "src/pages/gamingDashboard.html"),
+        labos: resolve(__dirname, "src/pages/labos.html"),
       },
     },
-    outDir: "../build",
+    outDir: "dist",
   },
   base: "/portfolio/",
 });
